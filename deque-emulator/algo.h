736d3fe20fa6ac0729160c0864d08796f8cd8d7f
yankoss67 <abanosimova.elena71@gmail.com>
2026-09-10 22:07:58 +0300

    Update algo.h

--- deque-emulator/algo.h
+++ deque-emulator/algo.h
@@ -4,11 +4,46 @@
 
 template<class T, class Comp>
 std::deque<T> Merge(const std::deque<T>& half1, const std::deque<T>& half2, const Comp& comparator) {
-    // Реализуйте слияние, используя компаратор.
+    std::deque<T> result;
+    size_t i = 0;
+    size_t j = 0;
+    
+    while (i < half1.size() && j < half2.size()) {
+        if (comparator(half1[i], half2[j])) {
+            result.push_back(half1[i]);
+            ++i;
+        } else {
+            result.push_back(half2[j]);
+            ++j;
+        }
+    }
+    
+    while (i < half1.size()) {
+        result.push_back(half1[i]);
+        ++i;
+    }
+    
+    while (j < half2.size()) {
+        result.push_back(half2[j]);
+        ++j;
+    }
+    
+    return result;
 }
 
 template<class T, class Comp>
 std::deque<T> MergeSort(const std::deque<T>& src, const Comp& comparator) {
-    // Реализуйте рекурсивную функцию MergeSort
-    // на основе задачи из урока о рекурсии.
+    if (src.size() <= 1) {
+        return src;
+    }
+    
+    size_t mid = src.size() / 2;
+    
+    std::deque<T> left(src.begin(), src.begin() + mid);
+    std::deque<T> right(src.begin() + mid, src.end());
+    
+    left = MergeSort(left, comparator);
+    right = MergeSort(right, comparator);
+    
+    return Merge(left, right, comparator);
 }
