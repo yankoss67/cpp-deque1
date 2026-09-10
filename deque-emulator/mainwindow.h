3d00c6d4d601aeef6b4f309d9ec301ae30d7633e
yankoss67 <abanosimova.elena71@gmail.com>
2026-09-10 21:00:13 +0300

    Update mainwindow.h

--- deque-emulator/mainwindow.h
+++ deque-emulator/mainwindow.h
@@ -1,3 +1,71 @@
 #pragma once
 
-// В качестве заготовки возьмите последний эмулятор вектора.
+#include <QMainWindow>
+#include <QStringListModel>
+#include <QListWidgetItem>
+#include <random>
+
+#include "model.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void SetRandomGen(const std::mt19937& random_gen);
+
+private slots:
+    // Методы дека
+    void on_btn_clear_clicked();
+    void on_btn_push_front_clicked();
+    void on_btn_push_back_clicked();
+    void on_btn_pop_back_clicked();
+    void on_btn_pop_front_clicked();
+    void on_btn_insert_clicked();
+    void on_btn_erase_clicked();
+    void on_btn_edit_clicked();
+    void on_btn_resize_clicked();
+
+    // Итератор
+    void on_btn_begin_clicked();
+    void on_btn_end_clicked();
+    void on_btn_dec_iterator_clicked();
+    void on_btn_inc_iterator_clicked();
+
+    // Заготовки
+    void on_btn_tea_clicked();
+    void on_btn_cakes_clicked();
+
+    // Алгоритмы
+    void on_btn_find_clicked();
+    void on_btn_count_clicked();
+    void on_btn_min_element_clicked();
+    void on_btn_max_element_clicked();
+    void on_btn_merge_sort_clicked();
+    void on_btn_merge_sort_case_insensitive_clicked();
+    void on_btn_shuffle_clicked();
+    void on_btn_unique_clicked();
+    void on_btn_reverse_clicked();
+
+    // Алгоритмы поиска
+    void on_btn_lower_bound_clicked();
+    void on_btn_upper_bound_clicked();
+
+    void on_list_widget_currentRowChanged(int currentRow);
+
+private:
+    void ApplyModel();
+    void ApplyIterator();
+
+private:
+    Model deque_model_;
+    Ui::MainWindow *ui;
+    std::mt19937 random_gen_;
+}; 
